eed113da23e1599344820c28428c60d65febfa4b
viktorJMarton <martonviktor666@gmail.com>
2023-02-01 10:05:27 +0000

    Activation email feature rebase

--- db/schema.rb
+++ db/schema.rb
@@ -57,10 +57,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_215920) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
